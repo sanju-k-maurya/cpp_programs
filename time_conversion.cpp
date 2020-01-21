64342dae5fd7977299bc6e1a29ae7f2ab7e701b3
Sanju <sanju.mca19.du@gmail.com>
2020-01-21 22:27:14 +0530

    Create time_conversion.cpp
    This code converts time entered in AM/PM format to 24 hour format.

---
+++ time_conversion.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	string input_time;
+	cout<<"Enter time in HH:MM:SSAM/HH:MM:SSPM format: ";
+	getline(cin,input_time);
+	int hour;
+	string hour_time;
+	string change_hour;
+	string hh,mm,ss;
+	int ihh,imm,iss;
+	hh=input_time.substr(0,2);
+	mm=input_time.substr(3,2);
+	ss=input_time.substr(6,2);
+	ihh=stoi(hh);
+	imm=stoi(mm);
+	iss=stoi(ss);
+	string output_time = input_time.substr(0,8);
+	if(ihh>=01 && ihh<=12 && imm>=00 && imm<=59 && iss>=00 && iss<=59)
+	{
+		if(input_time.substr(8,2)=="PM" && input_time.substr(0,2)!="12")
+		{
+			hour_time = input_time.substr(0,2);
+			hour = stoi(hour_time);
+			hour=hour+12;
+			change_hour= to_string(hour);
+			output_time[0]=change_hour[0];
+			output_time[1]=change_hour[1];
+
+
+		}
+		else if(input_time.substr(8,2)=="AM" && input_time.substr(0,2)=="12")
+		{
+			output_time[0]='0';
+			output_time[1]='0';
+
+		}
+
+		
+		cout<<"Time in 24 hour format is :";
+		cout<<output_time<<endl;
+	}
+
+}
