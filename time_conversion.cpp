da6b6288c28f0149f6a0055eff79ff23a7d38587
sanju-kumar-maurya <sanju.mca19.du@gmail.com>
2021-01-01 23:28:14 +0530

    Problem statement added

--- time_conversion.cpp
+++ time_conversion.cpp
@@ -1,4 +1,10 @@
 //This code converts time entered in AM/PM format to 24 hour format.
+/*
+Given a time in 12-hour AM/PM format, convert it to military (24-hour) time.
+
+Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
+- 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
+*/
 #include<iostream>
 using namespace std;
 int main()
